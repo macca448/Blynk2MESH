f7d09024aa2b3a9c7ceb9e1fb2ec2d11bc0dc6e3
macca-nz <20883620+macca448@users.noreply.github.com>
2023-03-14 09:18:48 +1300

    Update credentials.h

--- learning/blynk_tank_node/credentials.h
+++ learning/blynk_tank_node/credentials.h
@@ -1,12 +1,12 @@
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 
-#define BLYNK_TEMPLATE_ID "TMPLXnbUIUnd"
-#define BLYNK_DEVICE_NAME "ESP8266"
+#define BLYNK_TEMPLATE_ID "YOUR_ID"
+#define BLYNK_DEVICE_NAME "YOUR_TEMPLATE"
 
-char auth[] = "svs21j47kLaT8RaEJA09zsUxa5tfZvAO";
+char auth[] = "YOUR_KEY";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "MillFlat_El_Rancho";
-char pass[] = "140824500925";
+char ssid[] = "YOUR_SSID";
+char pass[] = "YOUR_PASSWORD";
