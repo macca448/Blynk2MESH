77428e32fcec5a99e12d9b26ad0e0a19e7279ce6
macca-nz <20883620+macca448@users.noreply.github.com>
2023-03-18 23:54:11 +1300

    Update credentials.h

--- current/blynk2mesh/credentials.h
+++ current/blynk2mesh/credentials.h
@@ -1,10 +1,10 @@
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-#define BLYNK_TEMPLATE_ID "TMPLl7D87tMZ"
-#define BLYNK_TEMPLATE_NAME "ESP32DEV3"
-#define BLYNK_AUTH_TOKEN "4tI8T1r6o1O7k_h7e3wm3jE9DAA05CDW"
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "YOUR_TOKEN"
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-const char* ssid = "MillFlat_El_Rancho";
-const char* pass = "140824500925";
+const char* ssid = "YOUR_SSID";
+const char* pass = "YOUR_PASSWORD";
