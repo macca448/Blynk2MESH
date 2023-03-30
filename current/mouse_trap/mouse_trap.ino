e5594f4d796c62321ede1ac5cec37f23dbb3eaf5
macca-nz <20883620+macca448@users.noreply.github.com>
2023-03-31 11:55:14 +1300

    Update mouse_trap.ino

--- current/mouse_trap/mouse_trap.ino
+++ current/mouse_trap/mouse_trap.ino
@@ -1,46 +1,44 @@
 //A simplistic first build
 //Both trap design and this sketch need more work and fine tuning
-//This is configured for the Uno/Nano so you can use the Serial monitor
-//Final Solution will be on an ATTiny85
+//Working Prototype on an ATTiny85
+//Set bootloader clock to "internal 8Mhz"
+//Tested with the Simple ATTiny25/45/85 controller library
 
 //8bit Servo library for ATTiny85
-//#include "Servo8Bit.h"                //For ATTiny85
-#include <Servo.h>                      //For testing using NANO/Uno
+#include "Servo8Bit.h"                //https://github.com/fri000/Servo8Bit
+
 //Input/Output pin constants
-#define PRINT                           //Only in testing on NANO/Uno otherwise comment out
-#define LED   13        //PB0 Pin 5     These are the ATTiny pins
-#define SERVO 11        //PB1 Pin 6
-#define ENTRY A1        //PB2 Pin 7
-#define TRAP  A2        //PB4 Pin 3
-#define STATE 3         //PB3 Pin 2
-//#define RESET 5       //PB5 Pin 1
+
+#define LED   PB0           //PB0 Pin 5     These are the ATTiny pins
+#define SERVO PB1           //PB1 Pin 6
+#define ENTRY A1            //PB2 Pin 7 A1
+#define TRAP  A2            //PB4 Pin 3 A2
+#define STATE PB3           //PB3 Pin 2
+                            //PB5 Pin 1 = RESET Must be Pulled High
 //Macro's
 #define OPEN 160        
-#define CLOSED 30
-#define DUTY 500     
+#define CLOSED 40
+#define DUTY 500  
+#define DELAY 4   
+
 //Servo object
-//Servo8Bit servo;                      //For ATTiny85
-Servo servo;                            //For testing using NANO/Uno  
+Servo8Bit servo;                      
+
 //All the variables
 uint32_t  previousTime = 0;       
 bool      update = 0, watch = 0, armed = 0, ledState = 0, blink = 0, udServo = 0;
 uint16_t  eReading, tReading, lastEreading, lastTreading;
-uint8_t   pos = OPEN, timer1 = 0, printTimer = 0;
+uint8_t   pos = OPEN, timer1 = 0;
 
 void setup(){
-  #ifdef PRINT
-      Serial.begin(115200);
-  #endif
   pinMode(LED, OUTPUT);
   digitalWrite(LED, ledState);
   pinMode(STATE, OUTPUT);
   digitalWrite(STATE, ledState);
-  //pinMode(ENTRY, INPUT);
-  //pinMode(TRAP, INPUT);
   servo.attach(SERVO);
   servo.write(pos);
   delay(3000);
-  //servo.detach();    
+  servo.detach();    
 }
 
 void loop (){
@@ -61,7 +59,8 @@ void loop (){
             if(watch){
                   if(tReading != lastTreading){
                       if(tReading > 500){          //Mouse now entering main chamber of trap
-                          //servo.attach(SERVO);    //Get ready to close the trap door
+                          servo.attach(SERVO);    //Get ready to close the trap door
+                          servo.write(OPEN);
                           timer1 = 0;             //Reset a counter for a small delay
                           armed = true;
                       }
@@ -71,7 +70,7 @@ void loop (){
                 if(tReading && eReading < 500 ){            //The mouse should now be in the main chamber                             
                     timer1++;
                 }  
-                if(timer1 >= 10 && (tReading && eReading < 500 )){  //5 second delay and mouse must not back in the entry
+                if(timer1 >= DELAY && (tReading && eReading < 500 )){  //5 second delay and mouse must not back in the entry
                     pos = CLOSED;                   //Set servo position
                     digitalWrite(STATE, HIGH);      //This will update BLYNK about the capture
                     timer1 = 0;                     //Start new timer
@@ -79,16 +78,6 @@ void loop (){
                     blink = true;                   //Blink the led to signal mouse in trap
                 }
             }
-
-            #ifdef PRINT
-                printTimer++;
-                if(printTimer >= 6){
-                    printTimer = 0;
-                    eReading = analogRead(ENTRY);
-                    tReading = analogRead(TRAP);
-                    Serial.println((String)"Entrance: " + eReading + "\tTrap: " + tReading + "\n");
-                }
-            #endif
             if(blink){
                 ledState = !ledState;
                 update = true;        
